bc221f6d82aa5803685abf24aca36475db2dd35e
Sushant Kumar <sushantprajapati1412@gmail.com>
2023-02-15 13:13:35 +0530

    chatting engine

--- assests/js/chat_engine.js
+++ assests/js/chat_engine.js
@@ -1,5 +1,4 @@
 
-
 class ChatEngine{
     constructor(chatBoxId, userEmail){
         this.chatBox = $(`#${chatBoxId}`);
@@ -28,5 +27,41 @@ class ChatEngine{
                 console.log('a user joined!', data.user_email)
             })
         });
+
+        // send a message on click the send messagge button
+        $('#send-message').click(function(){
+            let msg = $('#chat-message-input').val();
+
+            if(msg != ''){
+                self.socket.emit('send_message',{
+                    message:msg,
+                    user_email:self.userEmail,
+                    chatroom:'codeial'
+                })
+            }
+        });
+
+        self.socket.on('receive_message',function(data){
+            console.log('message received',data.message);
+
+            let newMessage= $('<li>');
+
+            let messageType = 'other-message';
+            if(data.user_email == self.userEmail){
+                messageType='self-message';
+            }
+
+            newMessage.append($('<span>',{
+                'html': data.message
+            }));
+
+            newMessage.append($('<sub>',{
+                'html':data.user_email
+            }));
+
+            newMessage.addClass(messageType);
+
+            $('#chat-messages-list').append(newMessage);
+        })
     }
 }
