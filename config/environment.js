c55da2d8549f53d850df76a2e8a948a07beda9b6
Sushant Kumar <sushantprajapati1412@gmail.com>
2023-02-23 22:10:58 +0530

    change development

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,7 @@ const { session } = require("passport")
 const fs=require('fs');
 const rfs=require('rotating-file-stream');
 const path=require('path');
+const development=require('./development');
 
 const logDirectory=path.join(__dirname,'../production_logs');
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
@@ -11,36 +12,11 @@ const accessLogStream = rfs.createStream('access.log',{
     path:logDirectory
 });
 
-const development={
-    name:'development',
-    assest_path:'./assests',
-    session_cookie_key:'blahsomething',
-    db:'deve_passport_del',
-    smtp:{
-        service:'gmail',
-        host:'smtp.gmail.com',
-        port:587,
-        secure:false,
-        auth:{
-            user: 'sushan14t@gmail.com',
-            pass: 'gvifhvwwyzgldjpi'
-        }
-    },
-    google_client_id:"751492571518-sk9sud1ebedkprngl80pomrs0vr9uig0.apps.googleusercontent.com",
-    google_client_secret:'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',
-    google_callback_url:'http://localhost:8007/users/auth/google/callback',
-    jwt_secret:'codeial',
-    morgan:{
-        mode:'dev',
-        options:{stream:accessLogStream}
-    }
-    
-}
+
 
 const production={
     name:'production',
     assest_path:process.env.CODIAL_ASSET_PATH,
-    // session_cookie_key:'5yoRvBiUKexcoigRKk26FqDzc44X8fpg',
     session_cookie_key:process.env.codeial_session_cookie,
     db:process.env.codeial_database,
     smtp:{
